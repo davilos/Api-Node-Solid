032dbb730a3c5d44812ecff35407ad655da5d483
davilos <daviloscostagg@hotmail.com>
2024-03-20 15:28:01 -0300

    feat: Adiciona o link para a aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import { env } from './env'
 import { app } from './app'
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-app.listen(
+void app.listen(
   {
     host: '0.0.0.0',
     port: env.PORT
   }
 ).then(
   () => {
-    console.log('✔ HTTP Server Running!')
+    console.log(`✔ HTTP Server is running on http://localhost:${env.PORT}`)
   }
 )
