4b218f0b5c45ea2dc1eb0d61fb39677021074ee7
davilos <daviloscostagg@hotmail.com>
2024-03-21 17:56:50 -0300

    feat: Adiciona a interface da response do execute

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,11 +1,16 @@
 import { type UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { type User } from '@prisma/client'
 
 interface RegisterServiceRequest {
   name: string
   email: string
   password: string
+}
+
+interface RegisterServiceResponse {
+  user: User
 }
 
 export class RegisterService {
@@ -15,7 +20,7 @@ export class RegisterService {
     name,
     email,
     password
-  }: RegisterServiceRequest): Promise<void> {
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
     const passwordHash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -24,10 +29,14 @@ export class RegisterService {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash: passwordHash
     })
+
+    return {
+      user
+    }
   }
 }
