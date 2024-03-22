c9e7face156916ef1fda0be91b375ccb2c51f123
davilos <daviloscostagg@hotmail.com>
2024-03-22 12:13:07 -0300

    test: Adiciona os testes de autenticação de user

---
+++ src/services/authenticate.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect, describe } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { AuthenticateService } from './authenticate'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+describe('Authenticate Service', () => {
+  test('A user can authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateService(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6)
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@example.com',
+      password: '123456'
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  test('A user can not authenticate with a wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateService(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6)
+    })
+
+    await expect(async () => await sut.execute({
+      email: 'johndoe1@example.com',
+      password: '123456'
+    })).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  test('A user can not authenticate with a wrong password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateService(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6)
+    })
+
+    await expect(async () => await sut.execute({
+      email: 'johndoe@example.com',
+      password: '1234567'
+    })).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
