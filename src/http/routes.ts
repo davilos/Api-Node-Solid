345d2a1359444de826099d5766be29774e7eeacf
davilos <daviloscostagg@hotmail.com>
2024-03-22 12:15:34 -0300

    feat: Adiciona a rota de autenticação do user

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,9 @@
 import { type FastifyInstance } from 'fastify'
 import { register } from './controllers/RegisterController'
+import { authenticate } from './controllers/AuthenticateController'
 
 export async function appRoutes (app: FastifyInstance): Promise<void> {
   app.post('/users', register)
+
+  app.post('/sessions', authenticate)
 }
