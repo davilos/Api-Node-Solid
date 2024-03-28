d5bc3379bf44cdf80b71f28cd250d20408ac4236
davilos <daviloscostagg@hotmail.com>
2024-03-28 16:19:31 -0300

    f097

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -4,6 +4,16 @@ import { type UsersRepository } from '../users-repository'
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
+  async findById (id: string): Promise<User | null> {
+    const user = this.items.find(item => item.id === id)
+
+    if (user === undefined) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail (email: string): Promise<User | null> {
     const user = this.items.find(item => item.email === email)
 
---
+++ src/services/get-user-profile.ts
@@ -0,0 +1,30 @@
+import { type UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+import { type User } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface GetUserProfileServiceRequest {
+  userId: string
+}
+
+interface GetUserProfileServiceResponse {
+  user: User
+}
+
+export class GetUserProfileService {
+  constructor (private readonly usersRepository: UsersRepository) {}
+
+  async execute ({ userId }: GetUserProfileServiceRequest):
+  Promise<GetUserProfileServiceResponse> {
+    const user = await this.usersRepository.findById(userId)
+
+    if (user === null) {
+      throw new ResourceNotFoundError()
+    }
+
+    return {
+      user
+    }
+  }
+}
