e396452f104640cd43a726d48daae27a3657d884
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-04 19:43:50 -0300

    feat: Adiciona dois métodos no repositório

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,6 +2,8 @@ import { type CheckIN, type Prisma } from "@prisma/client"
 
 export interface CheckInsRepository {
   create(data: Prisma.CheckINUncheckedCreateInput): Promise<CheckIN>
+  save(checkIn: CheckIN): Promise<CheckIN>
+  findById(id: string): Promise<CheckIN | null>
   findByUserIdOnDdate(userId: string, date: Date): Promise<CheckIN | null>
   findManyByUserId(userId: string, page: number): Promise<CheckIN[]>
   countByUserId(userId: string): Promise<number>
