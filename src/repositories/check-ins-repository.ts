224a768e0ec03f2d84ee4bcf58053c20485dc554
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-04 16:12:01 -0300

    feat: Adiciona um novo método ao repositório

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,4 +3,5 @@ import { type CheckIN, type Prisma } from "@prisma/client"
 export interface CheckInsRepository {
   create(data: Prisma.CheckINUncheckedCreateInput): Promise<CheckIN>
   findByUserIdOnDdate(userId: string, date: Date): Promise<CheckIN | null>
+  findManyByUserId(userId: string, page: number): Promise<CheckIN[]>
 }
