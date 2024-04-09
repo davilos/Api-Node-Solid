d4f1fa00b39c90f26591af6bdd6799676a734a2a
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-09 17:04:43 -0300

    refactor: Refatora a response do controller

--- src/http/controllers/ProfileController.ts
+++ src/http/controllers/ProfileController.ts
@@ -1,12 +1,20 @@
+import { makeGetUserProfileService } from "@/services/factories/make-get-user-profile-service"
 import { type FastifyRequest, type FastifyReply } from "fastify"
 
 export async function profile(
   request: FastifyRequest,
   response: FastifyReply,
 ): Promise<FastifyReply> {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileService()
 
-  console.log(request.user.sub)
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
 
-  return await response.status(200).send()
+  return await response.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
