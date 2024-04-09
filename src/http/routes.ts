a0626e398145ba996496f34bd9cd25bfb0e8f85d
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-09 17:05:38 -0300

    feat: Adiciona o middleware para cada request

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,6 +2,7 @@ import { type FastifyInstance } from "fastify"
 import { register } from "./controllers/RegisterController"
 import { authenticate } from "./controllers/AuthenticateController"
 import { profile } from "./controllers/ProfileController"
+import { verifyJWT } from "./middlewares/verify-jwt"
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post("/users", register)
@@ -9,5 +10,5 @@ export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post("/sessions", authenticate)
 
   // Authenticated
-  app.get("/me", profile)
+  app.get("/me", { onRequest: [verifyJWT] }, profile)
 }
