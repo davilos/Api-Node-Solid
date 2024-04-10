bb3ecaf822fce443672a9c318d2370ca7e003709
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 16:56:52 -0300

    feat: Implementa o controller de criação de CheckIn

---
+++ src/http/controllers/check-ins/CreateController.ts
@@ -0,0 +1,34 @@
+import { type FastifyRequest, type FastifyReply } from "fastify"
+import { z } from "zod"
+import { makeCheckInService } from "@/services/factories/make-check-in-service"
+
+export async function create(
+  request: FastifyRequest,
+  response: FastifyReply,
+): Promise<FastifyReply> {
+  const createCheckInParamsSchema = z.object({
+    gymId: z.string().uuid(),
+  })
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { gymId } = createCheckInParamsSchema.parse(request.params)
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body)
+
+  const checkInService = makeCheckInService()
+
+  await checkInService.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return await response.status(201).send()
+}
