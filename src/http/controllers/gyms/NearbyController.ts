89bafe07fe63d0db0e3c2c9b56e2ad142a6ab48d
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 16:26:07 -0300

    feat: Implementa o controller de buscar Gyms próximas

---
+++ src/http/controllers/gyms/NearbyController.ts
@@ -0,0 +1,30 @@
+import { type FastifyRequest, type FastifyReply } from "fastify"
+import { z } from "zod"
+import { makeFetchNearbyGymsService } from "@/services/factories/make-fetch-nearby-gyms-service"
+
+export async function nearby(
+  request: FastifyRequest,
+  response: FastifyReply,
+): Promise<FastifyReply> {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query)
+
+  const fetchNearbyGymsService = makeFetchNearbyGymsService()
+
+  const { gyms } = await fetchNearbyGymsService.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return await response.status(200).send({
+    gyms,
+  })
+}
