6512d7b9422846acb3f90e0cc59e2de752c83ed3
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 16:26:49 -0300

    feat: Implementa as rotas de Gyms

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
 import { FastifyInstance } from "fastify"
+import { search } from "./SearchController"
+import { nearby } from "./NearbyController"
+import { create } from "./CreateController"
 
 export async function gymsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook("onRequest", verifyJWT)
+
+  app.get("gyms/search", search)
+  app.get("gyms/nearby", nearby)
+
+  app.post("/gyms", create)
 }
