1560515772d3cfd5bbaa2024e56c364f4025b713
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 19:19:05 -0300

    feat: Adiciona o refreshToken
    O refreshToken é implementado como um cookie com expiração de 7 dias.

--- src/http/controllers/users/AuthenticateController.ts
+++ src/http/controllers/users/AuthenticateController.ts
@@ -31,9 +31,27 @@ export async function authenticate(
       },
     )
 
-    return await response.status(200).send({
-      token,
-    })
+    const refreshToken = await response.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      },
+    )
+
+    return await response
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return await response.status(400).send({ message: error.message })
