d3fdc1b51ffeeb261158eed6c57cc11cd8bcedf3
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 15:25:25 -0300

    move: Move os controllers para a pasta users

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,8 +1,8 @@
 import { type FastifyInstance } from "fastify"
-import { register } from "./controllers/RegisterController"
-import { authenticate } from "./controllers/AuthenticateController"
-import { profile } from "./controllers/ProfileController"
-import { verifyJWT } from "./middlewares/verify-jwt"
+import { register } from "./RegisterController"
+import { authenticate } from "./AuthenticateController"
+import { profile } from "./ProfileController"
+import { verifyJWT } from "@/http/middlewares/verify-jwt"
 
 export async function appRoutes(app: FastifyInstance): Promise<void> {
   app.post("/users", register)
