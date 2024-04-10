a670f4b35e663d7dce7d7218fa63532c15232c98
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 16:59:42 -0300

    feat: Implementa as rotas de CheckIn

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,6 +1,16 @@
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
 import { FastifyInstance } from "fastify"
+import { create } from "./CreateController"
+import { validate } from "./ValidateController"
+import { metrics } from "./MetricsController"
+import { history } from "./HistoryController"
 
 export async function checkInsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook("onRequest", verifyJWT)
+
+  app.get("/check-ins/history", history)
+  app.get("/check-ins/metrics", metrics)
+
+  app.post("/gyms/:gymId/check-ins", create)
+  app.patch("/check-ins/:checkInId/validate", validate)
 }
