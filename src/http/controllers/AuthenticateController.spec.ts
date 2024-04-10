7c1f3f85ac067821f5f869b6b1550f72035f648f
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-10 14:57:12 -0300

    test: Implementa o teste de autenticação de User

--- src/http/controllers/AuthenticateController.spec.ts
+++ src/http/controllers/AuthenticateController.spec.ts
@@ -1,3 +1,30 @@
-import { test } from "vitest"
+import request from "supertest"
+import { app } from "@/app"
+import { afterAll, beforeAll, describe, expect, test } from "vitest"
 
-test("ok", () => {})
+describe("Authenticate (E2E)", () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+
+  test("Can be able to authenticate.", async () => {
+    await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    })
+
+    const response = await request(app.server).post("/sessions").send({
+      email: "johndoe@example.com",
+      password: "123456",
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
