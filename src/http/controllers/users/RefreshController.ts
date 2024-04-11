efca0ca3e2842f34bbd0efe0af51506c387fc228
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-11 17:25:25 -0300

    feat: Adiciona o cargo no payload de User

--- src/http/controllers/users/RefreshController.ts
+++ src/http/controllers/users/RefreshController.ts
@@ -6,8 +6,10 @@ export async function refresh(
 ): Promise<FastifyReply> {
   await request.jwtVerify({ onlyCookie: true })
 
+  const { role } = request.user
+
   const token = await response.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
@@ -16,7 +18,7 @@ export async function refresh(
   )
 
   const refreshToken = await response.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
