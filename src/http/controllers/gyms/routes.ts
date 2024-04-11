137e4ae4b5cc6ebd59cd1ad6e74d815f43849eb3
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-11 17:27:45 -0300

    feat: Middleware de verificação de cargo de User

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from "fastify"
 import { search } from "./SearchController"
 import { nearby } from "./NearbyController"
 import { create } from "./CreateController"
+import { verifyUserRole } from "@/http/middlewares/verify-user-role"
 
 export async function gymsRoutes(app: FastifyInstance): Promise<void> {
   app.addHook("onRequest", verifyJWT)
@@ -10,5 +11,5 @@ export async function gymsRoutes(app: FastifyInstance): Promise<void> {
   app.get("/gyms/search", search)
   app.get("/gyms/nearby", nearby)
 
-  app.post("/gyms", create)
+  app.post("/gyms", { onRequest: [verifyUserRole("ADMIN")] }, create)
 }
