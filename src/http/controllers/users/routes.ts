a067c2bcb9446ee65bbfbdfe4050b2b125c19bb1
Dávilos Tavares <daviloscostagg@hotmail.com>
2024-04-11 16:50:16 -0300

    feat: Adiciona a rota de token refresh

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { register } from "./RegisterController"
 import { authenticate } from "./AuthenticateController"
 import { profile } from "./ProfileController"
 import { verifyJWT } from "@/http/middlewares/verify-jwt"
+import { refresh } from "./RefreshController"
 
 export async function usersRoutes(app: FastifyInstance): Promise<void> {
   app.post("/users", register)
-
   app.post("/sessions", authenticate)
+
+  app.patch("/token/refresh", refresh)
 
   // Authenticated
   app.get("/me", { onRequest: [verifyJWT] }, profile)
